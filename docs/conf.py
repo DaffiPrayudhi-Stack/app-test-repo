46ea1d1ca4fc0e7f94457baa1ee74b8b6b906b88
DaffiPrayudhi-Stack <daffi.prayudhi@sinarsaktimetalindo.com>
2025-09-02 14:58:17 +0700

    .

--- docs/conf.py
+++ docs/conf.py
@@ -1,20 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# Configuration file for the Sphinx documentation builder.
-
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# http://www.sphinx-doc.org/en/stable/config
+import os
+import sys
 
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+# add these directories to sys.path here.
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
@@ -22,25 +15,23 @@ project = 'app-test-repo-doc'
 copyright = '2025, Daffi Prayudhi'
 author = 'Daffi Prayudhi'
 
+# The full version, including alpha/beta/rc tags
+release = '1.0'
+
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+]
+
+# TAMBAHKAN MASTER DOC SETTING:
+master_doc = 'index'
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-# html_theme = 'alabaster'
-
-# -- Options for manual page output ------------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    ('index', 'app-test-repo', 'app-test-repo Documentation',
-     ['Daffi Prayudhi'], 1)
-]
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,18 @@
+app-test-repo Documentation
+===========================
+
+Welcome to the documentation for app-test-repo.
+
+Contents
+--------
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
